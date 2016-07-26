10c0d72f5c6ba46c161d53d34d705a23e2a1fce7
Jacqueline Phan <jtphan14@gmail.com>
2016-07-25 17:36:11 -0700

    Add lucky num method, list num method

--- luckynumber.rb
+++ luckynumber.rb
@@ -1,4 +1,20 @@
 input = ARGV
 
-min_num = ARGV[0]
-max_num = ARGV[1]
+min_num = ARGV[0].to_i
+max_num = ARGV[1].to_i
+
+def list_num(min_num, max_num)
+  for i in min_num..max_num
+  end
+end
+
+def lucky_num?(num)
+  return true if num == 7
+end
+
+def sum_num(num)
+  num.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+list_num(min_num, max_num)
+puts sum_num(100)
