e58f77dcccd3127fe0e2409983c99afe7abc0276
Jacqueline Phan <jtphan14@gmail.com>
2016-07-25 21:34:18 -0700

    Add recursion method for sum of numbers

--- luckynumber.rb
+++ luckynumber.rb
@@ -11,10 +11,23 @@ end
 def lucky_num?(num)
   return true if num == 7
 end
-
+#
+def recursion(num)
+  return num if  num.to_s.length == 1
+  recursion(sum_num(num))
+end
+#
+# def
+#   until sum_num(num).length == 1
+#     num = sum_num(num)
+#     sum_num(num)
+#   end
+#
+#   end
+# end
 def sum_num(num)
   num.to_s.chars.map(&:to_i).reduce(:+)
 end
 
 list_num(min_num, max_num)
-puts sum_num(100)
+puts recursion(sum_num(62431))
