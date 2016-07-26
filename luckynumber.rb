5b55134aca2a8262aa368ebb63dcf0211ed9f172
Jacqueline Phan <jtphan14@gmail.com>
2016-07-25 21:52:43 -0700

    Complete list-num methods

--- luckynumber.rb
+++ luckynumber.rb
@@ -5,29 +5,21 @@ max_num = ARGV[1].to_i
 
 def list_num(min_num, max_num)
   for i in min_num..max_num
+    puts "#{i}" if lucky_num?(single_digit_sum(sum_numbers(i)))
   end
 end
 
 def lucky_num?(num)
   return true if num == 7
 end
-#
-def recursion(num)
+
+def single_digit_sum(num)
   return num if  num.to_s.length == 1
-  recursion(sum_num(num))
+  single_digit_sum(sum_numbers(num))
 end
-#
-# def
-#   until sum_num(num).length == 1
-#     num = sum_num(num)
-#     sum_num(num)
-#   end
-#
-#   end
-# end
-def sum_num(num)
+
+def sum_numbers(num)
   num.to_s.chars.map(&:to_i).reduce(:+)
 end
 
 list_num(min_num, max_num)
-puts recursion(sum_num(62431))
