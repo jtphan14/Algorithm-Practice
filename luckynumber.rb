26dc374d9b9da4ddec98f5ca89bbfacd16e3af9c
Jacqueline Phan <jtphan14@gmail.com>
2016-07-25 21:59:44 -0700

    Update codes to pass linter methods

--- luckynumber.rb
+++ luckynumber.rb
@@ -1,11 +1,9 @@
-input = ARGV
-
 min_num = ARGV[0].to_i
 max_num = ARGV[1].to_i
 
 def list_num(min_num, max_num)
-  for i in min_num..max_num
-    puts "#{i}" if lucky_num?(single_digit_sum(sum_numbers(i)))
+  (min_num..max_num).each do |i|
+    puts i.to_s if lucky_num?(single_digit_sum(sum_numbers(i)))
   end
 end
 
